9c63005bf531e0e41b09a8f74c3f0ba85d952186
Antonio Perez Ochoa <antonio.perezochoa1@gmail.com>
2023-01-26 08:17:25 -0700

    cleaned up code

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -1,29 +1,17 @@
 USE employees;
 # 2)
-# SELECT * FROM employees
-#   WHERE first_name IN ('Irena', 'Vidya', 'Maya')
-#   ORDER BY first_name ASC;
+# SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY first_name;
 
 # 3)
-# SELECT * FROM employees
-#   WHERE first_name IN ('Irena', 'Vidya', 'Maya')
-#   ORDER BY first_name ASC, last_name ASC;
+# SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY first_name, last_name;
 
 # 4)
-# SELECT * FROM employees
-#     WHERE first_name IN ('Irena', 'Vidya', 'Maya')
-#     ORDER BY last_name ASC, first_name ASC;
+# SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY last_name, first_name;
 
 # 5)
-# SELECT * FROM employees
-#     WHERE last_name LIKE 'E%'
-#     ORDER BY emp_no ASC;
+# SELECT * FROM employees WHERE last_name LIKE 'E%' ORDER BY emp_no;
 
 # 6)
-SELECT * FROM employees
-WHERE first_name IN ('Irena', 'Vidya', 'Maya')
-ORDER BY last_name DESC, first_name DESC;
+SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY last_name DESC, first_name DESC;
 
-SELECT * FROM employees
-WHERE last_name LIKE 'E%'
-ORDER BY emp_no DESC;
+SELECT * FROM employees WHERE last_name LIKE 'E%' ORDER BY emp_no DESC;
